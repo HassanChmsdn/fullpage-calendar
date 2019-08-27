25436dfefeb164b60cd645f39cda399255726759
Hassan Chamseddine <hassan@crowdee.de>
2019-08-27 22:49:59 +0200

    adding project files

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { extend } from '@syncfusion/ej2-base';
+import { EventSettingsModel, DayService, WeekService, WorkWeekService, MonthService, AgendaService, ResizeService, DragAndDropService } from '@syncfusion/ej2-angular-schedule';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    providers: [DayService, WeekService, WorkWeekService, MonthService, AgendaService, ResizeService, DragAndDropService]
+})
+export class AppComponent {
+    public selectedDate: Date = new Date(2019, 0, 10);
+    public eventSettings: EventSettingsModel = { dataSource: <Object[]>extend([], null, true) };
+}
